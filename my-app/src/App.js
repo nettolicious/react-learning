1418f65b952a133330f14a62f93411c20a59cd4a
Matthew Morgan <matthew.morgan@nettolicious.com>
2020-01-30 16:21:58 -0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const collection = [
+  { name: 'Bodhi' },
+  { name: 'Mary' },
+  { name: 'Marie' }
+];
+
+function HelloComponent({ name, ...props }) {
+  return <h1 {...props}>Hello {name}!</h1>;
+}
+
 function App() {
+  let [index, setIndex] = React.useState(0);
+  let person = collection[index];
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +30,15 @@ function App() {
         >
           Learn React
         </a>
+        <button type="button" onClick={() => setIndex(index + 1)}>Next</button>
+        {
+          person ? (
+            <HelloComponent name={person.name} />
+          ) : (
+            <h1>Nobody</h1>
+          )
+        }
+        
       </header>
     </div>
   );
